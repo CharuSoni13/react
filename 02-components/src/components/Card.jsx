3d8a70f2e725d188325bbc8f8a4516b643dc1950
CharuSoni13 <sonicharu2004@gmail.com>
2026-05-27 12:06:22 +0530

    componets and props

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className = 'card'>
+      <h1>Charu Soni</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, aspernatur!</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h3>Charu Soni</h3>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
